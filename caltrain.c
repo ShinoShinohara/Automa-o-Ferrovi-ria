f812282d34f97e12327b7fa852e2d16a990add74
ShinoShinohara <vshinohara@outlook.com>
2022-10-02 17:03:31 -0400

    cond for seats and passengers

--- caltrain.c
+++ caltrain.c
@@ -8,4 +8,8 @@ void station_init(struct station *station)
     station->passageiros_entraram = 0;
 
     pthread_mutex_init(&(station->mutex), NULL);
+
+    // p/ saber se os passageiros estao em seus assentos && todos os lugares do trem estejam ocupados ou não existam mais passageiros para embarcar
+    pthread_cond_init(&(station->assentos_disponiveis), NULL);
+    pthread_cond_init(&(station->passageiros_entraram), NULL);
 }
--- station.h
+++ station.h
@@ -12,10 +12,14 @@ struct station
     pthread_mutex_t mutex;
 };
 
+// Quando um trem chega na estação ele invocará a função station_load_train(struct station *station, int count), cujo segundo argumento indica o número de vagas disponíveis no trem.Essa função não deve encerrar sua execução até que : todos os passageiros estejam em seus assentos e todos os lugares do trem estejam ocupados ou não existam mais passageiros para embarcar.
+// count é o numero de vagas disponiveis
 void station_load_train(struct station *station, int count);
 
+// Quando um passageiro chega na estação ele invocará a função station_wait_for_train(struct station *station) Essa função não deve encerrar sua execução até que : exista um trem na estação(isto é, há uma execução da função station_load_train em andamento) e existam vagas o bastante para esse passageiro sentar - se no trem.
 void station_wait_for_train(struct station *station);
 
+// Ao se acomodar em seu assento o passageiro invocará a função station_on_board(struct station *station), para notificar ao trem que já embarcou.
 void station_on_board(struct station *station);
 
 #endif
